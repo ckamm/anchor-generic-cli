8ba65f85665d9069e9c00ce9c1f30247b9dfe891
Christian Kamm <mail@ckamm.de>
2021-12-15 17:44:25 +0100

    Extract buildInstruction helper

--- src/anchor.ts
+++ src/anchor.ts
@@ -1,4 +1,5 @@
 import * as anchor from "@project-serum/anchor";
+import { parseAccountArg, parseArg } from "./util";
 
 export async function anchorSetup(
   rpcUrl: string,
@@ -15,4 +16,65 @@ export async function anchorSetup(
   const idl = await anchor.Program.fetchIdl(programId, provider);
   const program = new anchor.Program(idl, programId, provider);
   return [idl, program];
+}
+
+export async function buildInstruction(
+  idl: anchor.Idl,
+  instructionName: string,
+  kvArgsList: [string, string][],
+  programId: anchor.web3.PublicKey
+): Promise<[any[], Object]> {
+  let kvArgs = Object.fromEntries(kvArgsList);
+  const initialKvArgs = { ...kvArgs };
+
+  const instruction = idl.instructions.find((i) => i.name === instructionName);
+  if (!instruction) {
+    throw new Error("instruction not found in program");
+  }
+
+  // Bind all accounts
+  let accounts = {};
+  for (const accountItem of instruction.accounts) {
+    // TODO: deal with composite accounts, probably do "composite.accountname=foo"
+    // but composites can nest...
+    // The correct way to test here would need IdlAccount / IdlAccounts exported.
+    // @ts-ignore
+    if (accountItem.accounts) {
+      throw new Error("composite fields in accounts are not supported");
+    }
+
+    const accountName = accountItem.name;
+    if (!(accountName in kvArgs)) {
+      throw new Error('missing argument for account "' + accountName + '"');
+    }
+    const value = kvArgs[accountName];
+    delete kvArgs[accountName];
+
+    // @ts-ignore
+    const isSigner = accountItem.isSigner;
+    accounts[accountName] = await parseAccountArg(
+      accountName,
+      value,
+      isSigner,
+      programId
+    );
+  }
+
+  // Bind all fields
+  let args = [];
+  for (const arg of instruction.args) {
+    if (!(arg.name in kvArgs)) {
+      throw new Error('missing argument for argument "' + arg.name + '"');
+    }
+    const value = kvArgs[arg.name];
+    delete kvArgs[arg.name];
+
+    args.push(await parseArg(arg.type, value, initialKvArgs, programId));
+  }
+
+  if (Object.keys(kvArgs).length > 0) {
+    throw new Error("unexpected arguments: " + Object.keys(kvArgs).join(", "));
+  }
+
+  return [args, { accounts }];
 }
--- src/handleInstruction.ts
+++ src/handleInstruction.ts
@@ -1,73 +1,18 @@
 import * as anchor from "@project-serum/anchor";
-import { parseAccountArg, parseArg } from "./util";
-import { anchorSetup } from "./anchor";
+import { anchorSetup, buildInstruction } from "./anchor";
 
 export async function handleInstruction({
   programId,
   rpcUrl,
   instructionName,
   kvArgsList,
-  signers,
 }) {
   const [idl, program] = await anchorSetup(rpcUrl, programId);
-
-  let kvArgs = Object.fromEntries(kvArgsList);
-  const initialKvArgs = { ...kvArgs };
-
-  if (!(instructionName in program.instruction)) {
-    throw new Error("instruction not found in program");
-  }
-
-  const instruction = idl.instructions.find((i) => i.name === instructionName);
-  if (!instruction) {
-    throw new Error("instruction not found in program");
-  }
-
-  // Bind all accounts
-  let accounts = {};
-  for (const accountItem of instruction.accounts) {
-    // TODO: deal with composite accounts, probably do "composite.accountname=foo"
-    // but composites can nest...
-    // The correct way to test here would need IdlAccount / IdlAccounts exported.
-    // @ts-ignore
-    if (accountItem.accounts) {
-      throw new Error("composite fields in accounts are not supported");
-    }
-
-    const accountName = accountItem.name;
-    if (!(accountName in kvArgs)) {
-      throw new Error('missing argument for account "' + accountName + '"');
-    }
-    const value = kvArgs[accountName];
-    delete kvArgs[accountName];
-
-    // @ts-ignore
-    const isSigner = accountItem.isSigner;
-    accounts[accountName] = await parseAccountArg(
-      accountName,
-      value,
-      isSigner,
-      programId
-    );
-  }
-
-  // Bind all fields
-  let args = [];
-  for (const arg of instruction.args) {
-    if (!(arg.name in kvArgs)) {
-      throw new Error('missing argument for argument "' + arg.name + '"');
-    }
-    const value = kvArgs[arg.name];
-    delete kvArgs[arg.name];
-
-    args.push(await parseArg(arg.type, value, initialKvArgs, programId));
-  }
-
-  if (kvArgs.length > 0) {
-    throw new Error(
-      "unexpected arguments: " + kvArgs.map((kv) => kv.key).join(", ")
-    );
-  }
-
-  console.log(program.instruction[instructionName](...args, { accounts }));
+  const [args, ctx] = await buildInstruction(
+    idl,
+    instructionName,
+    kvArgsList,
+    programId
+  );
+  console.log(program.instruction[instructionName](...args, ctx));
 }
